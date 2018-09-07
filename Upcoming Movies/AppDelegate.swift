0fc0346dc683189cddf2015aa420a0d3aad502c4
Matheus Oliveira Rabelo <matheus.oliveira.rabelo@gmail.com>
2018-09-07 12:44:48 -0300

    Added project files

---
+++ Upcoming Movies/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Upcoming Movies
+//
+//  Created by Matheus Oliveira Rabelo on 9/7/18.
+//  Copyright © 2018 Matheus Rabelo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Upcoming Movies/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Upcoming Movies
+//
+//  Created by Matheus Oliveira Rabelo on 9/7/18.
+//  Copyright © 2018 Matheus Rabelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
